d976b109264af016f71f362cb3c5a5c57bb55834
Chance Smith <chancesmithb@gmail.com>
2017-04-20 14:42:04 -0500

    slows down initalization of box, reagrange inital call

--- main.js
+++ main.js
@@ -2,8 +2,6 @@
 var link = 'http://attheco.com',
     image = 'http://www.sodiumhalogen.com/up_b/thecotoberfestpopup_960-iz9HkwmEZS.png';
 
-showNotificationBox(link, image);
-
 function showNotificationBox(link, image) {
     /* create the promo box div */
 	if ($.cookie('theCO-notify') === null || $.cookie('theCO-notify') === ""
@@ -11,7 +9,7 @@ function showNotificationBox(link, image) {
         $('body').prepend("<div id='notification-bar' style='display:none; position: fixed; z-index: 9999;bottom:5px;right:5px;'><span id='close' style='position: absolute;top: -2px;right: 0px;opacity: .3;font-family: helvetica;font-size: 12px;padding: 5px;'>x</span><a href='" + link + "'><img src='" + image + "'/></a></div>");
 
         /* animate the promo */
-        $('#notification-bar').slideDown();
+        $('#notification-bar').slideDown(1200);
     } else {
     	console.log('cookie is set, no ad this time');
     }
@@ -22,8 +20,10 @@ $('span').click(function () {
 	$(this).parent().slideUp();
     /* set cookie date */
     var date = new Date();
-	var minutes = 60 * 24 * 3; //3 days
-    date.setTime(date.getTime() + (minutes * 60 * 1000));
+	var days = 3;
+    date.setTime(date.getTime() + (24 * 60 * 60 * 1000));
     /*set cookie*/
 	$.cookie("theCO-notify", "close", { expires: date });
 });
+
+showNotificationBox(link, image);
